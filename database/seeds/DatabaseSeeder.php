493f6b9a1592216ac223a0cebf36e11fe2996684
makoto2222 <lets_talk_about_feeling@yahoo.co.jp>
2020-11-26 22:45:29 +0000

    add: DB構築後 Migrate&Seederファイルの作成と実行

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call([
+            M_ShipmentsStatusesTableSeeder::class,
+            T_OrdersDetailsTableSeeder::class,
+            T_OrdersTableSeeder::class,
+        ]);
     }
 }
