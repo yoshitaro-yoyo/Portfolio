74dd9e6942c3f57b172ddae84aa38a3bdbf62ed9
vagrant <ju121.prog.acc.0925@gmail.com>
2020-12-05 06:46:06 +0000

    add:フロント ユーザ登録画面

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,9 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.register');
 });
+
+//ユーザ登録
+Route::get('signup','Auth\RegisterController@showRegistrationForm')->name('signup');
+Route::post('signup','Auth\RegisterController@register')->name('signup.post');
