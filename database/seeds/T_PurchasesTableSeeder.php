43765c6f031596e36ad38c6799696b2f73e9edc8
yoshitaro-yoyo <tsute3842@gmail.com>
2020-12-23 15:14:16 +0000

    Seederファイル内の配列化

--- database/seeds/T_PurchasesTableSeeder.php
+++ database/seeds/T_PurchasesTableSeeder.php
@@ -12,28 +12,30 @@ class T_purchasesTableSeeder extends Seeder
     public function run()
     {
         DB::table('t_purchases')->insert([
-            'purchase_price' => '1000', 
-            'purchase_quantity' => '10', 
-            'purchase_company' => 'じゃがりこ株式会社', 
-            'order_date' => date('Y-m-d H:i:s'), 
-            'purchase_date' => date('Y-m-d H:i:s'), 
-            'product_id' => '1', 
-        ]);
-        DB::table('t_purchases')->insert([
-            'purchase_price' => '2000', 
-            'purchase_quantity' => '20', 
-            'purchase_company' => 'じゃがりこ2株式会社', 
-            'order_date' => date('Y-m-d H:i:s'), 
-            'purchase_date' => date('Y-m-d H:i:s'), 
-            'product_id' => '2', 
-        ]);
-        DB::table('t_purchases')->insert([
-            'purchase_price' => '3000', 
-            'purchase_quantity' => '30', 
-            'purchase_company' => 'じゃがりこ3株式会社', 
-            'order_date' => date('Y-m-d H:i:s'), 
-            'purchase_date' => date('Y-m-d H:i:s'), 
-            'product_id' => '3', 
+            [
+                'purchase_price' => '1000', 
+                'purchase_quantity' => '10', 
+                'purchase_company' => 'じゃがりこ株式会社', 
+                'order_date' => date('Y-m-d H:i:s'), 
+                'purchase_date' => date('Y-m-d H:i:s'), 
+                'product_id' => '1', 
+            ], 
+            [
+                'purchase_price' => '2000', 
+                'purchase_quantity' => '20', 
+                'purchase_company' => 'じゃがりこ2株式会社', 
+                'order_date' => date('Y-m-d H:i:s'), 
+                'purchase_date' => date('Y-m-d H:i:s'), 
+                'product_id' => '2', 
+            ], 
+            [
+                'purchase_price' => '3000', 
+                'purchase_quantity' => '30', 
+                'purchase_company' => 'じゃがりこ3株式会社', 
+                'order_date' => date('Y-m-d H:i:s'), 
+                'purchase_date' => date('Y-m-d H:i:s'), 
+                'product_id' => '3', 
+            ], 
         ]);
     }
 }
