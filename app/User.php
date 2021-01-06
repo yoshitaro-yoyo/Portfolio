45e870f37a524a96a229c53ec74852bc6b89d9d7
yoshitaro-yoyo <tsute3842@gmail.com>
2021-01-06 10:42:26 +0000

    add:Productモデルhasmanyリレーション追加

--- app/User.php
+++ app/User.php
@@ -44,4 +44,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function products()
+    {
+        return $this->hasmany('Product::class');
+    }
 }
