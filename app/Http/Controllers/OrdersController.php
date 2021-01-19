1f16584da06c36670690ac55cd5d1ebf32b9cf81
Juri-O <ju121.prog.acc.0925@gmail.com>
2021-01-19 10:54:48 +0000

    update:注文履歴修正210119

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -16,31 +16,10 @@ class OrdersController extends Controller
     public function index(Request $request)
     {
         $user = $request->user();
-        $orders = $user->orders()->orderBy('order_date','desc')->paginate(15);
-        return view('shopping.order_history',compact('user','orders'));
+        $orders = $user->orders()->orderBy('order_date', 'desc')->paginate(15);
+        return view('shopping.order_history', compact('user', 'orders'));
     }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
+    
     /**
      * Display the specified resource.
      *
@@ -51,41 +30,7 @@ class OrdersController extends Controller
     {
         $user = User::find($id);
         $targetDate = today()->subMonth(3);
-        $recentlyOrders = $user->orders()->where('order_date','>',$targetDate)->orderBy('order_date','desc')->paginate(15);
-        return view('shopping.search_order_history',compact('user','recentlyOrders'));
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        $recentlyOrders = $user->orders()->where('order_date', '>', $targetDate)->orderBy('order_date', 'desc')->paginate(15);
+        return view('shopping.search_order_history', compact('user', 'recentlyOrders'));
     }
 }
