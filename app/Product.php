5289e3f4c9adfc687439773d1872d7dc99fa0e23
yoshitaro-yoyo <tsute3842@gmail.com>
2021-01-22 15:34:42 +0000

    modi:ProductModelの修正

--- app/Product.php
+++ app/Product.php
@@ -4,21 +4,21 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+
 class Product extends Model
 {
     protected $table = 'm_products';
-
     public $timestamps = false;
 
     //カテゴリーIDに関連する商品カテゴリー情報を取得する
     public function category()
     {
-        return $this->belongsTo('Category::class');
+        return $this->belongsTo('App\Category');
     }
 
     //ユーザ情報にProduct情報を紐付ける
     public function users()
     {
-        return $this->belongsTo('User::class');
+        return $this->belongsTo('App\User');
     }
 }
